d3cbe0dd9caba8aad117365ad2e67732e8049148
Becky <becky.ruan524@gmail.com>
2021-02-22 18:49:49 -0800

    Recreated Tables to add start and end date

--- Tables/dbo.UserToProject.sql
+++ Tables/dbo.UserToProject.sql
@@ -1,6 +1,6 @@
-CREATE TABLE dbo.UserToProject
-(
-	Id bigint not null primary key identity(1, 1),
-	AccountId bigint not null foreign key references dbo.Account(Id), 
-	ProjectId bigint not null foreign key references dbo.Project(ProjectId)
+CREATE TABLE dbo.UserToProject 
+( 
+	Id bigint not null primary key identity(1, 1), 
+	AccountId bigint not null foreign key references dbo.Account(Id),  
+	ProjectId bigint not null foreign key references dbo.Project(ProjectId) 
 );
