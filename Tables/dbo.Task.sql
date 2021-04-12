749c3c46d62fb13ac4cd4a8e4134f4e2b9ffd2db
Becky <becky.ruan524@gmail.com>
2021-04-12 15:00:21 -0700

    Update to task table

--- Tables/dbo.Task.sql
+++ Tables/dbo.Task.sql
@@ -4,6 +4,7 @@ CREATE TABLE dbo.Task
     TaskName varchar(50) not null,
 	TaskDescription varchar(max) not null,
 	ProjectId bigint Foreign key references dbo.Project(ProjectId) not null,
+	TaskerId bigint Foreign key references dbo.Account(Id) not null,
 	CreatedDate datetime2(7) not null default getdate(),
 	UpdatedDate datetime2(7) not null default getdate(),
 	CompletedDate datetime2(7) not null,
