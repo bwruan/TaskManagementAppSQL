aceec702e5339b93b87c902429fe00048288d317
Becky <becky.ruan524@gmail.com>
2021-05-18 08:16:12 -0700

    edit task table

--- Tables/dbo.Task.sql
+++ Tables/dbo.Task.sql
@@ -4,11 +4,10 @@ CREATE TABLE dbo.Task
     TaskName varchar(50) not null,
 	TaskDescription varchar(max) not null,
 	ProjectId bigint Foreign key references dbo.Project(ProjectId) not null,
-	TaskerId bigint Foreign key references dbo.Account(Id) not null,
 	TaskeeId bigint Foreign key references dbo.Account(Id) not null,
 	IsCompleted bit default 0 not null,
 	CreatedDate datetime2(7) not null default getdate(),
 	UpdatedDate datetime2(7) not null default getdate(),
-	CompletedDate datetime2(7) not null default getdate(),
+	CompletedDate datetime2(7),
 	DueDate datetime2(7) not null
 );
