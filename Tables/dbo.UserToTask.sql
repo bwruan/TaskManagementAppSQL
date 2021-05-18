9c68f56edd661e7ef14d8c2aa356ab09cf940fc0
Becky <becky.ruan524@gmail.com>
2021-05-17 18:13:36 -0700

    deleted UserToTask table

--- Tables/dbo.UserToTask.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE dbo.UserToTask 
-(
-    Id bigint PRIMARY KEY Identity(1,1) NOT NULL,
-    AccountId bigint Foreign key references dbo.Account(Id) not null,
-	TaskId bigint Foreign key references dbo.Task(TaskId) not null
-);
